a030d9859d5033b62c91c40e89678de0ee157338
Artem <artemmohov05@mail.ru>
2025-04-02 14:21:09 +0500

    Заготовка перехода по страницам

--- PP_01_02/MainWindow.xaml.cs
+++ PP_01_02/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static PP_01_02.MainWindow;
 
 namespace PP_01_02
 {
@@ -16,9 +18,32 @@ namespace PP_01_02
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            OpenPages(pages.main);
+        }
+
+        public enum pages
+        {
+            main
+        }
+
+        public void OpenPages(pages _pages)
+        {
+            this.MinHeight = 800;
+            this.MinWidth = 950;
+            this.Height = 850;
+            this.Width = 1000;
+
+            switch (_pages)
+            {
+                case pages.main:
+                    frame.Navigate(new Pages.Main());
+                    break;
+            }
         }
     }
 }
