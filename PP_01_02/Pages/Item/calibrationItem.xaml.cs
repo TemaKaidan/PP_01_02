745bc5973ebf2f37bb2d25fe1ddbff1aa19a2672
Artem <artemmohov05@mail.ru>
2025-04-03 16:15:10 +0500

    Реализована генерация отчета по калибровкам

--- PP_01_02/Pages/Item/calibrationItem.xaml.cs
+++ PP_01_02/Pages/Item/calibrationItem.xaml.cs
@@ -1,18 +1,19 @@
-﻿using PP_01_02.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using iText.IO.Font;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using PP_01_02.Context;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Kernel.Font;
+using iText.Kernel.Colors;
+using System.IO;
+using iText.IO.Font;
 
 namespace PP_01_02.Pages.Item
 {
@@ -64,6 +65,69 @@ namespace PP_01_02.Pages.Item
             {
                 MessageBox.Show("Произошла ошибка.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        private void GenerateReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<Models.calibration> calibrations;
+            using (var context = new calibrationContext())
+            {
+                calibrations = context.calibration.ToList();
+            }
+            GeneratePdfWithTable(calibrations);
+        }
+
+        public void GeneratePdfWithTable(List<Models.calibration> calibrations)
+        {
+            string filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Отчет по калибровке.pdf");
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                PdfWriter writer = new PdfWriter(fs);
+                PdfDocument pdf = new PdfDocument(writer);
+                Document document = new Document(pdf);
+
+                PdfFont font = PdfFontFactory.CreateFont("c:/windows/fonts/arial.ttf", PdfEncodings.IDENTITY_H);
+
+                document.Add(new iText.Layout.Element.Paragraph("Отчет по калибровке оборудования")
+                    .SetFont(font)
+                    .SetFontSize(18)
+                    .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER));
+
+                iText.Layout.Element.Table table = new iText.Layout.Element.Table(5);
+                table.SetHorizontalAlignment(iText.Layout.Properties.HorizontalAlignment.CENTER);
+
+                table.AddHeaderCell(new Cell().Add(new iText.Layout.Element.Paragraph("№").SetFont(font)));
+                table.AddHeaderCell(new Cell().Add(new iText.Layout.Element.Paragraph("Оборудование").SetFont(font)));
+                table.AddHeaderCell(new Cell().Add(new iText.Layout.Element.Paragraph("Сотрудник").SetFont(font)));
+                table.AddHeaderCell(new Cell().Add(new iText.Layout.Element.Paragraph("Результат калибровки").SetFont(font)));
+                table.AddHeaderCell(new Cell().Add(new iText.Layout.Element.Paragraph("Дата").SetFont(font)));
+
+                int rowNumber = 1;
+                foreach (var calibration in calibrations)
+                {
+                    var equipmentName = _equipmentContext.equipment.FirstOrDefault(x => x.equipment_id == calibration.equipment_id)?.name ?? "Неизвестно";
+
+                    var employeeName = "Неизвестно";
+                    employeesContext _employeesContext = new employeesContext();
+                    var employee = _employeesContext.employees.FirstOrDefault(x => x.employee_id == calibration.calibrated_by);
+                    if (employee != null)
+                    {
+                        employeeName = $"{employee.last_name} {employee.name} {employee.sur_name}";
+                    }
+
+                    table.AddCell(new Cell().Add(new iText.Layout.Element.Paragraph(rowNumber.ToString()).SetFont(font)));
+                    table.AddCell(new Cell().Add(new iText.Layout.Element.Paragraph(equipmentName).SetFont(font)));
+                    table.AddCell(new Cell().Add(new iText.Layout.Element.Paragraph(employeeName).SetFont(font)));
+                    table.AddCell(new Cell().Add(new iText.Layout.Element.Paragraph(calibration.calibration_result).SetFont(font)));
+                    table.AddCell(new Cell().Add(new iText.Layout.Element.Paragraph(calibration.calibration_date.ToString()).SetFont(font)));
+
+                    rowNumber++;
+                }
+                document.Add(table);
+                document.Close();
+            }
+            MessageBox.Show($"PDF файл был успешно создан: {filePath}");
         }
     }
 }
